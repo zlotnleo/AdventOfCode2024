9c683cc218c101209d8e4121cee17db085295850
Leo Zlotnikov <Leo.Zlotnikov@softwire.com>
2024-12-01 05:34:44 +0000

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,18 @@
+﻿var firstList = new List<int>();
+var secondList = new List<int>();
+foreach (var line in File.ReadAllLines("input.txt"))
+{
+    var values = line.Split("   ");
+    firstList.Add(int.Parse(values[0]));
+    secondList.Add(int.Parse(values[1]));
+}
+
+firstList.Sort();
+secondList.Sort();
+
+var part1 = firstList.Zip(secondList, (first, second) => Math.Abs(first - second)).Sum();
+Console.WriteLine(part1);
+
+var secondListCounts = secondList.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+var part2 = firstList.Select(x => x * secondListCounts.GetValueOrDefault(x, 0)).Sum();
+Console.WriteLine(part2);
