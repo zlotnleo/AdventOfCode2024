cf7289ce89f9694c23a3310b5dc8928aaece8696
Leo Zlotnikov <Leo.Zlotnikov@softwire.com>
2024-12-03 10:37:35 +0000

    Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+var memory = File.ReadAllText("input.txt");
+
+var part1 = 0;
+var part2 = 0;
+
+var isEnabled = true;
+for (var match = InstructionRegex().Match(memory); match.Success; match = match.NextMatch())
+{
+    switch (match.ValueSpan)
+    {
+        case "don't()":
+            isEnabled = false;
+            break;
+        case "do()":
+            isEnabled = true;
+            break;
+        default:
+            var product = int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+            part1 += product;
+            if (isEnabled)
+            {
+                part2 += product;
+            }
+
+            break;
+    }
+}
+
+Console.WriteLine(part1);
+Console.WriteLine(part2);
+
+internal partial class Program
+{
+    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)|do(n't)?\(\)")]
+    private static partial Regex InstructionRegex();
+}
