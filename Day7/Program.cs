13c8caa04a287140f295fb7bb10b70bd22d781d8
Leo Zlotnikov <Leo.Zlotnikov@softwire.com>
2024-12-07 18:27:01 +0000

    Day 7

---
+++ Day7/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Immutable;
+
+var equations = File.ReadAllLines("input.txt")
+    .Select(line => line.Split(' '))
+    .Select(split => (
+        Target: long.Parse(split[0][..^1]),
+        Numbers: ImmutableStack.CreateRange(split.Skip(1).Select(long.Parse))
+    ));
+
+var equationsByPossibility = equations.ToLookup(
+    eq => IsPossible(eq.Target, eq.Numbers, false)
+);
+var sumOfPossibleTargets = equationsByPossibility[true].Sum(eq => eq.Target);
+var part1 = sumOfPossibleTargets;
+Console.WriteLine(part1);
+
+var sumOfTargetsMadePossibleByConcatenation = equationsByPossibility[false]
+    .Where(eq => IsPossible(eq.Target, eq.Numbers, true))
+    .Sum(eq => eq.Target);
+
+var part2 = sumOfPossibleTargets + sumOfTargetsMadePossibleByConcatenation;
+Console.WriteLine(part2);
+
+return;
+
+bool IsPossible(long target, ImmutableStack<long> numbers, bool allowConcat)
+{
+    numbers = numbers.Pop(out var lastNumber);
+    if (numbers.IsEmpty)
+    {
+        return target == lastNumber;
+    }
+
+    return target >= lastNumber && IsPossible(target - lastNumber, numbers, allowConcat)
+           || target % lastNumber == 0 && IsPossible(target / lastNumber, numbers, allowConcat)
+           || allowConcat && TryDecatenateNumber(target, lastNumber, out var nextTarget) && IsPossible(nextTarget, numbers, allowConcat);
+}
+
+bool TryDecatenateNumber(long target, long number, out long nextTarget)
+{
+    while (number > 0)
+    {
+        if (number % 10 != target % 10)
+        {
+            nextTarget = -1;
+            return false;
+        }
+
+        number /= 10;
+        target /= 10;
+    }
+
+    nextTarget = target;
+    return true;
+}
